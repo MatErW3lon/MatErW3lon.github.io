ffe4379d37c163582f01bcf9b5567c9dcc82e119
Muhammad Rowaha <ashfaqrowaha@gmail.com>
2022-07-20 23:58:46 +0500

    fixed the nav-bar with web animation api

--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -1,51 +1,47 @@
 export function init(){
+    
     const button = document.querySelector('.nav-menu');
     const menu = document.querySelector('.menu');
     const banner = document.getElementById('banner');
-    let rollingStatus = true; //true-> roll Out, false-> roll Back
-
-    const check = document.querySelector('.item-1 #main-heading');
-
     
-    function getNumericalValue(fromString){
-        let numerical = fromString.substring(0, fromString.length - 2);
-        return parseFloat(numerical);
-    }
-
+    
+    let rollingStatus = true; //true-> roll Out, false-> roll Back
+ 
     var initRoll = (event) => {
         if(rollingStatus){
             banner.style.opacity = '0.8';
             button.innerHTML = '<img id="nav-menu" src="./resources/images/bx-right-arrow-alt.svg"/>';
-            var timer = setInterval(() => {
-                let widthOfMenu = window.getComputedStyle(menu).getPropertyValue('width');
-                let widthOfMenuFloat = getNumericalValue(widthOfMenu);
-                if(widthOfMenuFloat >= 150){
-                    rollingStatus = false;
-                    clearInterval(timer);
-                }else{
-                    widthOfMenuFloat += 5;
-                    widthOfMenu = widthOfMenuFloat + 'px';
-                    menu.style.width = widthOfMenu;
+            menu.animate([
+                {
+                    width: '0vw'
+                },
+                {
+                    width: '35vw',
+                    animationPlayState: 'paused'
                 }
-            },5);
+            ],{
+                duration: 250,
+                iterations: 1
+            });
+            menu.style.width = '35vw';
+            rollingStatus  = false;
         }else{
             banner.style.opacity = '1';
             button.innerHTML = '<img id="nav-menu" src="./resources/images/bx-menu.svg"/>';
-            var timer = setInterval(() => {
-                let widthOfMenu = window.getComputedStyle(menu).getPropertyValue('width');
-                let widthOfMenuFloat = getNumericalValue(widthOfMenu);
-                if(widthOfMenuFloat <= 0){
-                    rollingStatus = true;
-                    clearInterval(timer);
-                }else{
-                    widthOfMenuFloat -= 5;
-                    if(widthOfMenuFloat < 0){
-                        widthOfMenuFloat = 0;
-                    }
-                    widthOfMenu = widthOfMenuFloat + 'px';
-                    menu.style.width = widthOfMenu;
+            menu.animate([
+                {
+                    width: '35vw'
+                },
+                {
+                    width: '0vw',
+                    animationPlayState: 'paused'
                 }
-            },5);
+            ],{
+                duration: 100,
+                iterations: 1
+            });
+            menu.style.width = '0vw';
+            rollingStatus = true;
         }
 
     }

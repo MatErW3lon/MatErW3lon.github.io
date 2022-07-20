8db3801ebcb5b5ffc4a7a79d51649f38e09f2a23
Muhammad Rowaha <ashfaqrowaha@gmail.com>
2022-07-20 21:55:44 +0500

    working side nav bar but listener breaking after two callbacks

--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -2,29 +2,54 @@ export function init(){
     const button = document.querySelector('.nav-menu');
     const menu = document.querySelector('.menu');
     const banner = document.getElementById('banner');
+    let rollingStatus = true; //true-> roll Out, false-> roll Back
 
     const check = document.querySelector('.item-1 #main-heading');
+
+    
     function getNumericalValue(fromString){
-        let numerical = fromString.substring(0, fromString.length -2);
+        let numerical = fromString.substring(0, fromString.length - 2);
         return parseFloat(numerical);
     }
 
+    var initRoll = (event) => {
+        if(rollingStatus){
+            banner.style.opacity = '0.8';
+            button.innerHTML = '<img id="nav-menu" src="./resources/images/bx-right-arrow-alt.svg"/>';
+            var timer = setInterval(() => {
+                let widthOfMenu = window.getComputedStyle(menu).getPropertyValue('width');
+                let widthOfMenuFloat = getNumericalValue(widthOfMenu);
+                if(widthOfMenuFloat >= 150){
+                    rollingStatus = false;
+                    clearInterval(timer);
+                }else{
+                    widthOfMenuFloat += 5;
+                    widthOfMenu = widthOfMenuFloat + 'px';
+                    menu.style.width = widthOfMenu;
+                }
+            },5);
+        }else{
+            banner.style.opacity = '1';
+            button.innerHTML = '<img id="nav-menu" src="./resources/images/bx-menu.svg"/>';
+            var timer = setInterval(() => {
+                let widthOfMenu = window.getComputedStyle(menu).getPropertyValue('width');
+                let widthOfMenuFloat = getNumericalValue(widthOfMenu);
+                if(widthOfMenuFloat <= 0){
+                    rollingStatus = true;
+                    clearInterval(timer);
+                }else{
+                    widthOfMenuFloat -= 5;
+                    if(widthOfMenuFloat < 0){
+                        widthOfMenuFloat = 0;
+                    }
+                    widthOfMenu = widthOfMenuFloat + 'px';
+                    menu.style.width = widthOfMenu;
+                }
+            },5);
+        }
 
-    button.addEventListener('click', () => {
-        banner.style.opacity = '0.8';
-        button.innerHTML = '<img id="nav-menu" src="./resources/images/bx-right-arrow-alt.svg"/>';
-        var timer = setInterval(() => {
-            let widthOfMenu = window.getComputedStyle(menu).getPropertyValue('width');
-            check.innerHTML = widthOfMenu;
-            if(widthOfMenu === '35vw'){
-                clearInterval(timer);
-            }else{
-                let widthOfMenuFloat = getNumericalValue(heightOfMenu);
-                widthOfMenuFloat += 1;
-                widthOfMenu = widthOfMenuFloat + 'vw';
-                menu.style.width = widthOfMenu;
-            }
-        },10)}
-    );
+    }
+
+    button.addEventListener('click', initRoll);
 };
 

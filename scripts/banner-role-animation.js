ef349e8b2a5a699acad261668e8ac3bdd94451e0
Muhammad Rowaha <ashfaqrowaha@gmail.com>
2022-07-20 17:16:35 +0500

    added the scripts dir and banner role animation

---
+++ scripts/banner-role-animation.js
@@ -0,0 +1,35 @@
+const role1 = document.getElementById('role-1');
+const roles = ['Computer Engineering Student', 'Full Stack Developer', 'Freelancer'];
+let rolesIndex = 1; // not sure why this works but setting it to zero repeats the 0 index twice
+let opacityState = true; //true -> 1; false -> 0
+
+
+function fadeAnimation(){
+    let opacity = window.getComputedStyle(role1).getPropertyValue('opacity');
+    let opacityNum = parseFloat(opacity);
+    if(opacityState){
+        opacityNum -= 0.1;
+        opacity = opacityNum.toString();
+        role1.style.opacity = opacity;
+        if(opacity === '0'){
+            opacityState = false;
+            role1.innerHTML = roles[rolesIndex];
+            rolesIndex = rolesIndex + 1;
+            if(rolesIndex >= roles.length){
+                rolesIndex = 0;
+            }
+            targetedRoleIndex++;
+        }
+    }else{
+        opacityNum += 0.1;
+        opacity = opacityNum.toString();
+        role1.style.opacity = opacity;
+        if(opacity === '1'){
+            opacityState = true;
+        }
+    }   
+}
+
+var timer = setInterval(fadeAnimation, 100);
+
+
